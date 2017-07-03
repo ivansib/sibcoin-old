d0546dedfef51c18a5187c21cc9920ba6ccda272
UdjinM6 <UdjinM6@users.noreply.github.com>
2017-07-03 16:14:07 +0300

    various trivial cleanup fixes (#1501)

--- src/init.cpp
+++ src/init.cpp
@@ -1296,8 +1296,8 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
             return InitError(strError);
 
 
-    // Initialize KeePass Integration
-    keePassInt.init();
+        // Initialize KeePass Integration
+        keePassInt.init();
 
     } // (!fDisableWallet)
 #endif // ENABLE_WALLET
