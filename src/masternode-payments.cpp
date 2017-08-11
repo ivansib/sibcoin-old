c498070d64af9dbc3fa5b375053f13ccc9bfdb00
UdjinM6 <UdjinM6@users.noreply.github.com>
2017-08-11 21:50:57 +0300

    Pass reference when calling HasPayeeWithVotes (#1569)

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -527,7 +527,7 @@ bool CMasternodeBlockPayees::GetBestPayee(CScript& payeeRet)
     return (nVotes > -1);
 }
 
-bool CMasternodeBlockPayees::HasPayeeWithVotes(CScript payeeIn, int nVotesReq)
+bool CMasternodeBlockPayees::HasPayeeWithVotes(const CScript& payeeIn, int nVotesReq)
 {
     LOCK(cs_vecPayees);
 
