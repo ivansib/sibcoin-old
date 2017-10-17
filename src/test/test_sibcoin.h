70e1a03e0bc781b9c71bf426de25454a946c8d70
TriKriSta <Trikrista13@gmail.com>
2017-10-17 20:09:08 +0300

    rename test_dash -> test_sibcoin

--- src/test/test_sibcoin.h
+++ src/test/test_sibcoin.h
@@ -1,5 +1,5 @@
-#ifndef BITCOIN_TEST_TEST_DASH_H
-#define BITCOIN_TEST_TEST_DASH_H
+#ifndef BITCOIN_TEST_TEST_SIBCOIN_H
+#define BITCOIN_TEST_TEST_SIBCOIN_H
 
 #include "chainparamsbase.h"
 #include "key.h"
