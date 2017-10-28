7c8b8d6f51eb2a8a46c6c05281b510f245137a05
Alex <voyager3m@gmail.com>
2017-10-28 10:43:46 +0300

    change debug out to log file

--- src/dex/dexmanager.cpp
+++ src/dex/dexmanager.cpp
@@ -49,7 +49,7 @@ void CDexManager::ProcessMessage(CNode* pfrom, std::string& strCommand, CDataStr
                     pNode->PushMessage(NetMsgType::DEXOFFBCST, offer);
                 }
             }
-            LogPrint("dex", "DEXOFFBCST --\n%s\nfound %d\n", offer.dump().c_str(), bFound);
+            LogPrintf("DEXOFFBCST --\n%s\nfound %d\n", offer.dump().c_str(), bFound);
         }
     }
 }
