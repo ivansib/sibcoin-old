2475077b0f251f4f7db358a0b8fc8fc26268e223
rhino3333 <rhino1@tuta.io>
2017-11-01 16:48:27 -0700

    Added db version verification support to p2p

--- src/dex/include/dex/dexdb.h
+++ src/dex/include/dex/dexdb.h
@@ -7,6 +7,10 @@
 #include "dexdto.h"
 
 namespace dex {
+
+// Please update this DB version number if you change DB schema
+const unsigned int uiDexDBversionInCode = 1000;
+
 class DexDB {
 public:
     DexDB(const boost::filesystem::path &path);
@@ -17,14 +21,14 @@ public:
         All
     };
 
-    void addCountry(const std::string &iso, const std::string &name, const std::string &currency, const bool &enabled, const int &sortOrder);
-    void editCountry(const std::string &iso, const bool &enabled, const int &sortOrder);
+    void addCountry(const std::string &iso, const std::string &name, const std::string &currency, const bool &enabled = true);
+    void editCountry(const std::string &iso, const bool &enabled);
     void deleteCountry(const std::string &iso);
     std::list<CountryInfo> getCountriesInfo(const TypeView &type = All);
     CountryInfo getCountryInfo(const std::string &iso);
 
     void addCurrency(const std::string &iso, const std::string &name, const std::string &symbol, const bool &enabled, const int &sortOrder);
-    void editCurrency(const std::string &iso, const bool &enabled, const int &sortOrder);
+    void editCurrency(const std::string &iso, const bool &enabled);
     void deleteCurrency(const std::string &iso);
     std::list<CurrencyInfo> getCurrenciesInfo(const TypeView &type = All);
     CurrencyInfo getCurrencyInfo(const std::string &iso);
@@ -66,6 +70,9 @@ private:
     void addOrEditMyOffer(const std::string &query, const MyOfferInfo &offer);
     void bindOfferData(sqlite3pp::command &cmd, const OfferInfo &offer);
     bool isExistOffer(const std::string &tableName, const uint256 &idTransaction);
+    void addDbVersion( const int& uiDexDbVersion );
+    bool isDexDbOutdated();
+    void dropTables();
 
     sqlite3pp::database db;
 
