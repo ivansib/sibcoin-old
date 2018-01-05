e871090ae1e28313593c99378f0ac5b43501974b
TriKriSta <Trikrista13@gmail.com>
2018-01-05 16:37:15 +0200

    editing generate hash for offer

--- src/dex/dexoffer.h
+++ src/dex/dexoffer.h
@@ -90,12 +90,9 @@ public:
         READWRITE(countryIso);
         READWRITE(currencyIso);
         READWRITE(paymentMethod);
-        READWRITE(price);
         READWRITE(minAmount);
         READWRITE(timeCreate);
         READWRITE(timeExpiration);
-        READWRITE(shortInfo);
-        READWRITE(details);
     }
 
     uint256 MakeHash();
