a02232afd7de86c6e956e861935f9e5b256761ba
ivansib <ivansib@users.noreply.github.com>
2018-01-30 12:21:53 +0100

    update sqlite.mk

--- depends/packages/sqlite.mk
+++ depends/packages/sqlite.mk
@@ -1,16 +1,10 @@
 package=sqlite
-$(package)_WEBSITE=https://www.sqlite.org/
-$(package)_DESCR=SQLite
-$(package)_IGNORE=
-$(package)_VERSION=3210000
+$(package)_version=3210000
 $(package)_sha256_hash=d7dd516775005ad87a57f428b6f86afd206cb341722927f104d3f0cf65fbbbe3
-$(package)_SUBDIR=$(package)-autoconf-$($(package)_VERSION)
-$(package)_FILE=$(package)-autoconf-$($(package)_VERSION).tar.gz
-$(package)_URL=https://www.sqlite.org/2017/$($(package)_FILE)
-$(package)_DEPS=cc
+$(package)_file_name=$(package)-autoconf-$($(package)_version).tar.gz
+$(package)_download_path=https://www.sqlite.org/2017/
 
 define $(package)_preprocess_cmds
-  ./autogen.sh
 endef
 
 define $(package)_set_vars
